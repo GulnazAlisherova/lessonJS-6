79c8cfe7214d885f4794b4b2d6da532f9e09e675
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-14 14:28:59 +0600

    added document

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let a = document.createElement('a');
- let linkText = document.createTextNode("GOOGLE");
- a.appendChild(linkText);
- a.href = "https://google.com";
- document.body.appendChild(a);
-
- document.querySelector('a').addEventListener('dbclick', function(event){
-  console.log("YAHOO");
-});
+document.
