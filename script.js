f248c249945acc41ec8eca2f2d2bc822fd7fa113
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-14 15:11:11 +0600

    added фззутв

--- script.js
+++ script.js
@@ -10,14 +10,16 @@
 //   event.preventDefault();  
 //   this.remove();
 // })
-html.addEventListener('click', function(event) {
+
+
+document.addEventListener('click', function(event) {
   let div = document.createElement('div');
   div.style.display = 'inline';
   div.style.padding = `${Math.random()*50}px`;
   div.style.borderRadius = '100%';
   div.style.backgroundColor = `rgb(${Math.random()*250},${Math.random()*250},${Math.random()*250})`;
   div.style.position = 'absolute';
-  div.style.top =  event.clientY  + 'px' ;
-  div.style.left =  event.clientX  + 'px' ; //
-  body.append(div);
+  div.style.top =  event.clientY  + 'px' ; 
+  div.style.left =  event.clientX  + 'px' ; 
+  document.body.append(div);
 });
