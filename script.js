2be3303e61410315ebee43dc7d36c34035b1dc88
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-14 15:06:16 +0600

    added style left

--- script.js
+++ script.js
@@ -7,10 +7,17 @@
 //   this.style.background = "red";
 // });
 // document.querySelector('a').addEventListener('dblclick', function(event){
-//   event.preventDefault();
+//   event.preventDefault();  
 //   this.remove();
 // })
-
-document.querySelector('div').addEventListener('mousemove', function(event){
-  
+html.addEventListener('click', function(event) {
+  let div = document.createElement('div');
+  div.style.display = 'inline';
+  div.style.padding = `${Math.random()*50}px`;
+  div.style.borderRadius = '100%';
+  div.style.backgroundColor = `rgb(${Math.random()*250},${Math.random()*250},${Math.random()*250})`;
+  div.style.position = 'absolute';
+  div.style.top =  event.clientY  + 'px' ;
+  div.style.left =  event.clientX  + 'px' ; //
+  body.append(div);
 });
