a5a5d58b885bb1f7f054daa66e96db039feeb52f
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-14 14:33:18 +0600

    added remove

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.querySelector('a').addEventListener('click', function(event){
   event.preventDefault();
   this.textContent = "Yandex";
 });
+document.querySelector('a').addEventListener('contextmenu', function(event){
+  event.preventDefault();
+  this.style.background = "red";
+});
+document.querySelector('a').addEventListener('dblclick', function(event){
+  event.preventDefault();
+  this.remove();
+})
