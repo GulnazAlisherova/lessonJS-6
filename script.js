3208b39a4b3ffc3319622420740a779da39c8e71
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-15 21:54:30 +0600

    added let

--- script.js
+++ script.js
@@ -12,14 +12,34 @@
 // })
 
 
-document.addEventListener('click', function(event) {
-  let div = document.createElement('div');
-  div.style.display = 'inline';
-  div.style.padding = `${Math.random()*50}px`;
-  div.style.borderRadius = '100%';
-  div.style.backgroundColor = `rgb(${Math.random()*250},${Math.random()*250},${Math.random()*250})`;
-  div.style.position = 'absolute';
-  div.style.top =  event.clientY  + 'px' ; 
-  div.style.left =  event.clientX  + 'px' ; 
-  document.body.append(div);
-});
+// document.addEventListener('click', function(event) {
+//   let div = document.createElement('div');
+//   div.style.display = 'inline';
+//   div.style.padding = `${Math.random()*50}px`;
+//   div.style.borderRadius = '100%';
+//   div.style.backgroundColor = `rgb(${Math.random()*250},${Math.random()*250},${Math.random()*250})`;
+//   div.style.position = 'absolute';
+//   div.style.top =  event.clientY  + 'px' ; 
+//   div.style.left =  event.clientX  + 'px' ; 
+//   document.body.append(div);
+// });
+
+
+// let list = [
+//   'Apple', 'Banana', 'Cherry'
+// ];
+// let listElement =  document.createElement('ul');
+// document.body.prepend(listElement);
+// for (let i = 0; i < list.length; i++) {
+//   let listItemElement = document.createElement('li');
+//   listElement.append(listItemElement);
+//   listItemElement.textContent = list[i];
+//   listItemElement.addEventListener('click', function(event) {
+//     this.style.textDecoration = "line-through";
+//   });
+//   listItemElement.addEventListener('dblclick', function(event) {
+//     this.remove();
+//   });
+// };
+
+let 
